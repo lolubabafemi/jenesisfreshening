f459f1568d14e2dc913837dc5243bebc5308abf7
generaldraco <od.babafemi@gmail.com>
2024-06-17 08:57:08 +0100

    edit

--- script.js
+++ script.js
@@ -48,3 +48,23 @@ setInterval(() => {
     nextButton.click();
 }, 3000);
 
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.section');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('appear');
+            } else {
+                entry.target.classList.remove('appear');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
